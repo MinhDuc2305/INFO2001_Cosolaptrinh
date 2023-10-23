d74e2683e88df0382c8242e0a980b2a2389f100b
kSpyDuke <koda772.love@gmail.com>
2023-10-23 16:34:55 +0700

    Update test BTHH 2 - Bai 1

--- BTTH 2 - Bai1.c
+++ BTTH 2 - Bai1.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 int main (){
-    
-	//input
+    //input
 	int A, B, rs, du;
     printf ("Nhap gia tri A ('luu y A,B >= -1018 và A,b <= 1018')");
 	scanf ("%d %d", &A, &B);
-
-    //process
+	//process
 	rs=A/B;
     du=A%B;
-    
     //output
 	printf ("%d %d",rs,du);	
 	return 0;
